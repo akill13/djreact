39d58cbcc56d416826c6b28b180760e2500b2496
Abdul Halimi <abdulhalimi13@yahoo.com>
2020-02-03 23:19:20 -0500

    Added routes so that clicks on title will take you to detail page

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import BaseRouter from './routes';
 import 'antd/dist/antd.css';
 import CustomLayout from './containers/layout';
-import ArticleList from './containers/ArticleListView';
 
-function App() {
-  return (
-    <div className="App">
-      <CustomLayout>
-        <ArticleList />
-      </CustomLayout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <Router>
+          <CustomLayout>
+            <BaseRouter />
+          </CustomLayout>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
