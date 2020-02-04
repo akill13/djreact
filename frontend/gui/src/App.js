5227562654216baac1774b6314d5bfb1bbcdb9ff
Abdul Halimi <abdulhalimi13@yahoo.com>
2020-02-03 22:46:43 -0500

    Connected frontend with backend need to make it safer currently CORS is true

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './App.css';
 import 'antd/dist/antd.css';
 import CustomLayout from './containers/layout';
+import ArticleList from './containers/ArticleListView';
 
 function App() {
   return (
     <div className="App">
       <CustomLayout>
-
+        <ArticleList />
       </CustomLayout>
     </div>
   );
