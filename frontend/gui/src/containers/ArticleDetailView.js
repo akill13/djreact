28604a0a5e13539fe8f1ad9dce2f2538f6e5de18
Abdul Halimi <abdulhalimi13@yahoo.com>
2020-02-04 01:34:06 -0500

    Able to delete articles now

--- frontend/gui/src/containers/ArticleDetailView.js
+++ frontend/gui/src/containers/ArticleDetailView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Card } from 'antd';
+import { Button, Card } from 'antd';
 import CustomForm from '../components/Form';
 
 class ArticleDetail extends React.Component {
@@ -24,8 +24,16 @@ class ArticleDetail extends React.Component {
                     <p>{this.state.article.content}</p>
                 </Card>
                 <CustomForm requestType="put" articleID={this.props.match.params.articleID} btnText='Update' />
+                <form onSubmit={this.handleDelete}>
+                    <Button type='danger' htmlType='submit'>Delete</Button>
+                </form>
             </div>
         )
+    }
+
+    handleDelete = (event) => {
+        const articleID = this.props.match.params.articleID;
+        axios.delete(`http://127.0.0.1:8000/api/${articleID}/`);
     }
 }
 
