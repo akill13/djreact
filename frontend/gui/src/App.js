26cd81b71efc2d534cd80da33e2cb78a760d3d5f
Abdul Halimi <abdulhalimi13@yahoo.com>
2020-02-04 21:28:57 -0500

    Created authentication logic, this uses local storage to check a token and determine which action to take

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
 import BaseRouter from './routes';
 import 'antd/dist/antd.css';
 import CustomLayout from './containers/layout';
+import * as actions from './store/actions/auth';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
     return (
-      <div className="App" >
+      <div>
         <Router>
-          <CustomLayout>
+          <CustomLayout {...this.props}>
             <BaseRouter />
           </CustomLayout>
         </Router>
@@ -17,5 +24,15 @@ class App extends Component {
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  }
+}
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import ArticleList from './containers/ArticleListView';
 import ArticleDetail from './containers/ArticleDetailView';
+import Login from './containers/Login';
 const BaseRouter = () => (
     <div>
         <Route exact path='/' component={ArticleList} />
-        <Route exact path='/:articleID' component={ArticleDetail} />
+        <Route exact path='/:articleID/' component={ArticleDetail} />
+        <Route exact path='/login/' component={Login} />
     </div>
 );
 
